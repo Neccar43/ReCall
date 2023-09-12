2060bd64eb4bb0f31083f5aa0f166a6bc1d2b731
Abdulkerim Bilgi <akerimbilgi@gmail.com>
2023-09-12 11:32:11 +0300

    QuizResultScreen deleted.

--- app/src/main/java/com/novacodestudios/recall/presentation/quiz_result/QuizResultViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.novacodestudios.recall.presentation.quiz_result
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-@HiltViewModel
-class QuizResultViewModel@Inject constructor(): ViewModel() {
-}
